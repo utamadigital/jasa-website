783b0ae7f91f82fe7131fbae2322c32e2daf0508
utamadigital <utamadigitalid@gmail.com>
2026-01-13 09:43:53 +0700

    Remove accidental app copy folder

--- src/app - Copy/api/content/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { readSiteContent } from "@/lib/siteContent";
-export const runtime = "nodejs";
-
-export async function GET() {
-  return NextResponse.json(readSiteContent());
-}
